2291628d2069a8843d96dea6c46a15be327953c7
Khalid Sookia <khalidsookia@gmail.com>
2016-12-01 18:01:40 +0100

    dynamic call to appropriate action from route

---
+++ Controller/Controller.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by iKNSA.
+ * Author: Khalid Sookia <khalidsookia@gmail.com>
+ * Date: 01/12/16
+ * Time: 15:48
+ */
+
+namespace Romenys\Framework\Controller;
+
+
+class Controller
+{
+}
