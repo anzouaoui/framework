5fc7d7d2479d6be320c08c77a619173eaa9f632a
Khalid Sookia <khalidsookia@gmail.com>
2016-12-03 17:59:37 +0100

    added model hydrator

---
+++ Components/Model.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by iKNSA.
+ * Author: Khalid Sookia <khalidsookia@gmail.com>
+ * Date: 03/12/16
+ * Time: 17:51
+ */
+
+namespace Romenys\Framework\Components;
+
+
+class Model
+{
+    public function hydrate(array $data)
+    {
+        foreach ($data as $name => $value) {
+            $method = 'set'.ucfirst($name);
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+}
