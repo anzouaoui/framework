9097d680b2fa255efee85e26558a84d6aadeee8f
Khalid Sookia <khalidsookia@gmail.com>
2016-12-10 22:42:56 +0100

    added knp snappy and rendering template from mustach

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -9,6 +9,67 @@
 namespace Romenys\Framework\Controller;
 
 
+use Knp\Snappy\Pdf;
+
 class Controller
 {
+    private $snappy;
+
+    private $pdfBin = 'vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64';
+
+    public function __construct()
+    {
+        $this->setSnappy();
+    }
+
+    /**
+     * @return Pdf
+     */
+    public function getSnappy()
+    {
+        return $this->snappy;
+    }
+
+    /**
+     * @return Controller
+     */
+    public function setSnappy()
+    {
+        $this->snappy = new Pdf($this->getPdfBin());
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPdfBin()
+    {
+        return $this->pdfBin;
+    }
+
+    /**
+     * @param string pdfBin
+     *
+     * @return Controller
+     */
+    public function setPdfBin($pdfBin)
+    {
+        $this->pdfBin = $pdfBin;
+
+        return $this;
+    }
+
+    /**
+     * @param string $template absolute path to template
+     * @param array $data
+     *
+     * @return string
+     */
+    public function render($template, $data)
+    {
+        $mustache = new \Mustache_Engine;
+
+        return $mustache->render(file_get_contents($template), $data);
+    }
 }
